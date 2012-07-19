ab6df92dc27ae44d3030d2960b2d2c99bf88037d
fjgallardo@gmail.com <francis@jomer.(none)>
2012-07-19 19:53:30 +0800

    added template folder adjusted settings.py

--- AubreysAngels/AubreysAngels/oursite/views.py
+++ AubreysAngels/AubreysAngels/oursite/views.py
@@ -2,4 +2,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 
 def homepage(request):
-    return HttpResponse("Welcome to our homepage!")
+    # Loads the designed hompag template in template folder
+    t = loader.get_template('hompage.html')
+    c = Context({
+        
+    })
+
+    return HttpResponse(t.render(c))
+    #return HttpResponse("Welcome to our homepage!")
